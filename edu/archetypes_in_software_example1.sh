3fcd1557035dfe172a19a2267dad53b516965123
Ciprian-Virgil Gerstenberger <ciprian-virgil.gerstenberger@hnikt.no>
2025-12-19 05:57:11 +0000

    debugging

--- edu/archetypes_in_software_example1.sh
+++ edu/archetypes_in_software_example1.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CP="$GROOVY_HOME/lib/*:./lib/build/libs/openEHR-ADL-0.2.jar"
+CP="./lib/build/libs/openEHR-ADL-0.2.jar:/usr/local/sdkman/candidates/groovy/current/lib/*"
 java -cp "$CP" com.cabolabs.openehr.archetypes_in_software.Example1
+
